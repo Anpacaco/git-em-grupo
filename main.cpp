53c22b843785adee8406234dc7aa6c2efe8a7f81
ANTONIO PAULO CABRAL CONTENTE <antonio_contente@estudante.sesisenai.org.br>
2026-06-23 15:47:09 -0300

    Despedida feita

--- main.cpp
+++ main.cpp
@@ -5,9 +5,13 @@ void saudacao() {
 	cout << "Bem-vindo ao sistema!" << endl;
 }
 
+void despedida(){
+	cout<<"Tchau, obrigado por usar o programa."<< endl;
+}
+
 int main(){
 	saudacao();
-	cout<<"Projeto em grupo";
-	
+	cout<<"Projeto em grupo"<< endl;
+	despedida();
 	return 0;
 }
